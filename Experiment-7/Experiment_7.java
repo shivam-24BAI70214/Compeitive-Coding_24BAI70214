41a6b362ff36ec6a2553705ebf38c2313a1b6de9
shivam-24BAI70214 <shivam38.dt@gmail.com>
2026-04-06 07:22:09 +0530

    Implement word search algorithm in Experiment_7.java

---
+++ Experiment-7/Experiment_7.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public boolean exist(char[][] board, String word){
+        int rows=board.length;
+        int cols=board[0].length;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(dfs(board,word,i,j,0)) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int r, int c, int i) {
+        if(i==word.length()) return true;
+
+        if(r<0 || c < 0 || r>=board.length || c>=board[0].length || board[r][c]!=word.charAt(i)){
+            return false;
+        }
+
+        char temp=board[r][c];
+        board[r][c]='#';
+
+        boolean found=dfs(board,word,r+1,c,i+1) ||
+                      dfs(board,word,r-1,c,i+1) ||
+                      dfs(board,word,r,c+1,i+1) ||
+                      dfs(board,word,r,c-1,i+1);
+        board[r][c]=temp;
+        return found;
+    }
+}
